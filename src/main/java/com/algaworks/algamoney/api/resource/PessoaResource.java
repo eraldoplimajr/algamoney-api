5b633665332bf3f7a727767fb118e44ca432e3a0
eraldoplimajr <eraldoplimajr@gmail.com>
2022-01-09 00:25:10 -0300

    Usando eventos para adicionar header location

--- src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -1,12 +1,14 @@
 package com.algaworks.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoney.api.PessoaService;
+import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
 import com.algaworks.algamoney.api.model.Pessoa;
 
 @RestController
@@ -26,25 +28,33 @@ public class PessoaResource {
 	@Autowired
 	PessoaService pessoaService;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Pessoa> buscarTodasPessoas() {
 		return pessoaService.listarTodasPessoas();		
 	}
 	
 	@GetMapping("/{id}")
-	public Pessoa buscarPessoa(@PathVariable Long id) {
-		return pessoaService.buscarPessoaPorId(id).get();		
+	public ResponseEntity<Pessoa> buscarPessoa(@PathVariable Long id) {
+		
+		Optional<Pessoa> optPessoa = pessoaService.buscarPessoaPorId(id);
+		
+		if(!optPessoa.isPresent()) {
+			return ResponseEntity.notFound().build();		
+		}
+		
+		return ResponseEntity.ok(optPessoa.get());
 	}
 	
 	@PostMapping
 	public ResponseEntity<Pessoa> incluirPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaBD = pessoaService.incluirPessoa(pessoa);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaBD.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaBD.getCodigo()));
 		
-		return ResponseEntity.created(uri).body(pessoaBD);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaBD);
 	}
 	
 }

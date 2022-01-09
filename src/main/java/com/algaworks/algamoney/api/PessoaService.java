8caf674adb4b189cc7077eb0b8e07c2c6451937c
eraldoplimajr <eraldoplimajr@gmail.com>
2022-01-09 02:01:48 -0300

    Atualizando pessoas com put

--- src/main/java/com/algaworks/algamoney/api/PessoaService.java
+++ src/main/java/com/algaworks/algamoney/api/PessoaService.java
@@ -3,7 +3,9 @@ package com.algaworks.algamoney.api;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algamoney.api.model.Pessoa;
@@ -29,6 +31,22 @@ public class PessoaService {
 	
 	public void removerPessoa(Long codigo) {		
 		pessoaRepository.deleteById(codigo);		
+	}
+	
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {		
+		
+		Optional<Pessoa> optPessoa = pessoaRepository.findById(codigo);
+		
+		if(optPessoa.isEmpty()) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
+		Pessoa pessoaBD = optPessoa.get();
+		
+		BeanUtils.copyProperties(pessoa, pessoaBD, "codigo");
+		pessoaRepository.save(pessoaBD);
+		
+		return pessoaBD;
 	}
 
 }
